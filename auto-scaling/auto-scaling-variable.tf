9456a40061c1c707955ec0b50c21da728081d673
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-05 20:18:49 +0530

    Update auto-scaling-variable.tf

--- auto-scaling/auto-scaling-variable.tf
+++ auto-scaling/auto-scaling-variable.tf
@@ -1,195 +1,74 @@
 #LAUNCH CONFIGURATION VARIABLES
 
 variable "region" {
-  default = ""
+  default = "us-east-2"
 }
 
 variable "key-name" {
-  default = ""
+  default = "power"
 }
 
 variable "public-key-file-name" {
-  default = ""
+  default = "terra.pub"
 }
 
 variable "launch-configuration-name" {
-  default = ""
+  default = "asg"
 }
 
 variable "image-id" {
-  default = ""
+  default = "ami-0fb653ca2d3203ac1"
 }
 
 variable "instance-type" {
-  default = ""
+  default = "t2.micro"
 }
 
 variable "launch-configuration-security-groups" {
-  default = ""
+  default = "sg-0244e3f85210cc582"
 }
 
 variable "launch-configuration-public-key-name" {
-  default = ""
+  default = "terra"
 }
 
 
 #AUTO-SCALING VARIABLES
 
 variable "autoscaling-group-name" {
-  default = ""
+  default = "asg"
 }
 
 variable "max-size" {
-  default = ""
+  default = "2"
 }
 
 variable "min-size" {
-  default = ""
+  default = "1"
 }
 
 variable "health-check-grace-period" {
-  default = ""
+  default = "30"
 }
 
 variable "desired-capacity" {
-  default = ""
-}
-
-variable "force-delete" {
-  default = ""
+  default = "1"
 }
 
 variable "vpc-zone-identifier" {
   type = "list"
 }
 
-variable "target-group-arns" {
-  default = ""
-}
-
 variable "health-check-type" {
-  default = ""
+  default = "100"
 }
 
 variable "tag-key" {
-  default = ""
+  default = "terraf"
 }
 
 variable "tag-value" {
-  default = ""
-}
-
-#Auto-Scaling-Policy-Scale-up
-variable "auto-scaling-policy-name-scale-up" {
-  default = ""
-}
-
-variable "adjustment-type-scale-up" {
-  default = ""
-}
-
-variable "scaling-adjustment-scale-up" {
-  default = ""
-}
-
-variable "cooldown-scale-up" {
-  default = ""
-}
-
-variable "policy-type-scale-up" {
-  default = ""
-}
-
-#Auto-Scaling Policy Cloud-Watch Alarm-Scale-up
-variable "alarm-name-scale-up" {
-  default = ""
-}
-
-variable "comparison-operator-scale-up" {
-  default = ""
-}
-
-variable "evaluation-periods-scale-up" {
-  default = ""
-}
-
-variable "metric-name-scale-up" {
-  default = ""
-}
-
-variable "namespace-scale-up" {
-  default = ""
-}
-
-variable "period-scale-up" {
-  default = ""
-}
-
-variable "statistic-scale-up" {
-  default = ""
-}
-
-variable "threshold-scale-up" {
-  default = ""
-}
-
-variable "adjustment-type" {
-  default = ""
-}
-
-#Auto-Scaling Policy Scale-down
-
-variable "auto-scaling-policy-name-scale-down" {
-  default = ""
-}
-
-variable "adjustment-type-scale-down" {
-  default = ""
-}
-
-variable "scaling-adjustment-scale-down" {
-  default = ""
-}
-
-variable "cooldown-scale-down" {
-  default = ""
-}
-
-variable "policy-type-scale-down" {
-  default = ""
-}
-
-#Auto-Scaling Policy Cloud-Watch Alarm-Scale-down
-variable "alarm-name-scale-down" {
-  default = ""
-}
-
-variable "comparison-operator-scale-down" {
-  default = ""
-}
-
-variable "evaluation-periods-scale-down" {
-  default = ""
-}
-
-variable "metric-name-scale-down" {
-  default = ""
-}
-
-variable "namespace-scale-down" {
-  default = ""
-}
-
-variable "period-scale-down" {
-  default = ""
-}
-
-variable "statistic-scale-down" {
-  default = ""
-}
-
-variable "threshold-scale-down" {
-  default = ""
+  default = "terra1"
 }
 
 #Application Load Balancer Target Group
@@ -197,13 +76,13 @@ variable "alb-tg-name" {
   default = ""
 }
 variable "target-group-port" {
-  default = ""
+  default = "0"
 }
 
 variable "target-group-protocol" {
-  default = ""
+  default = "0"
 }
 
 variable "vpc-id" {
-  default = ""
+  default = "vpc-6014600b"
 }
